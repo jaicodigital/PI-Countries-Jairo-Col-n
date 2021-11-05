87a389fdb370e62c77603ef91befc22a92b4fae8
Jairo Colón <jaicodigital@gmail.com>
2021-11-04 22:36:44 -0500

    subiendo

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const axios = require("axios");
 const { Country } = require("./src/db.js");
-require('dotenv').config();
+const { DB_URL } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
@@ -47,7 +47,7 @@ conn.sync({ force: false }).then(async () => {
   } catch (error) {
     console.log(error);
   }
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
